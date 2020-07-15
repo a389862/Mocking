25a25b392e5a43b0d6b7a500c7b712826c3e2bda
Hari <r.hariprasad@gmail.com>
2020-07-15 18:10:48 +0530

    Initial Commit

---
+++ src/main/java/LearnMocking.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+
+public class LearnMocking {
+	
+	@BeforeMethod
+	public void setUpStub() {
+		stubFor(get(urlEqualTo("/api/issues"))
+				.willReturn(aResponse()
+						//.withHeader("Content-Type", "application/json")
+						.withStatus(200)
+						//.withBody("{\"desc\":\"good\"}")
+						.withBody("good")
+						));
+
+	}
+	
+	@Test
+	public void runGetMethod() {
+		RestAssured.baseURI ="http://localhost:9999/api/table/issues";
+		Response response = RestAssured.get();
+		response.prettyPrint();
+		
+		
+		
+
+	}
+
+}
